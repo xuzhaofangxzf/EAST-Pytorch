d67a899ebbfa5e94b420c7378b87bded4ad41b9a
pengyuanzhuo <pengyuanzhuo@163.com>
2019-04-11 17:29:10 +0800

    args

--- train.py
+++ train.py
@@ -53,26 +53,27 @@ def main():
     parser.add_argument('pretrain', metavar='PTH',
         help='pretrain model')
     parser.add_argument('-b', '--batch-size', type=int, default=14,
-        help='batch size per GPU')
+        help='batch size per GPU(default=14)')
     parser.add_argument('-l', '--lr', type=float, default=0.0001,
-        help='lr')
+        help='lr(default=0.0001)')
     parser.add_argument('-wd', type=float, default=1e-5,
-        help='weight decay')
-    parser.add_argument('--epochs', type=int, default=500,
-        help='epochs')
-    parser.add_argument('-j', '--num-workers', type=int, default=4,
-        help='dataloader workers')
+        help='weight decay(default=1e-5)')
+    parser.add_argument('--epochs', type=int, default=100,
+        help='epochs(default=100)')
+    parser.add_argument('-j', '--num-workers', type=int, default=16,
+        help='dataloader workers(default=16)')
     parser.add_argument('-s', '--input-size', type=int, default=512,
-        help='input image size(default 512)' +
+        help='input image size(default=512)' +
              'INPUT SIZE is the image size used by training,' +
              'it should be compatible with TEXT_SCALE')
     parser.add_argument('--text-scale', type=int, default=512,
         help='text_scale is the max text length EAST can detect,' +
-             'its restricted by the receptive field of CNN')
+             'its restricted by the receptive field of CNN' +
+             'default=512')
     parser.add_argument('--min-text-size', type=int, default=10,
-        help='min text size(default 10)')
+        help='min text size(default=10)')
     parser.add_argument('--gpus', type=str, default='0',
-        help='gpu ids')
+        help='gpu ids(default=0')
     parser.add_argument('--checkpoint', type=str, default='./checkpoint',
         help='checkpoint dir(default=./checkpoint)')
 
